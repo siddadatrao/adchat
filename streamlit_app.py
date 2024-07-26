11a5562b3233a95c672fe18a7b6cae2436e9fd1b
siddadatrao <sidd.adatrao@gmail.com>
2024-07-26 04:07:07 -0700

    Added base layer for chat side. Pulls from pinecone vector db gets similar and uses that for query

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,61 @@
 import streamlit as st
+import openai
 from openai import OpenAI
+from pinecone import Pinecone as PineconeClient
+import requests
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("AdChat Explore")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Ask this ad any questions you have!"
 )
+
+def connections(open_ai_key, pinecone_key):
+    pinecone = PineconeClient(api_key=pinecone_key)
+    open_ai_client = OpenAI(api_key=open_ai_key)
+    return open_ai_client, pinecone
+
+def get_embedding(text, open_ai_client, model="text-embedding-3-small"):
+   return open_ai_client.embeddings.create(input = [text], model=model).data[0].embedding
+
+def templatize(query, context):
+    return "Answer the question based only on the following context:" + query + "Question: " + query
+
+def get_similar(namespace, query, open_ai_client, pinecone):
+    query_embedding = get_embedding(query, open_ai_client, model='text-embedding-3-small')
+    query_results1 = pinecone.Index("adchat").query(
+        namespace=namespace,
+        vector=query_embedding,
+        top_k=1,
+        include_values=False,
+        include_metadata=True,
+    )
+    query_result = query_results1['matches'][0]['metadata']['text']
+    return templatize(query_result, query)
+
+def get_completion(prompt, key):
+    openai.api_key = key
+
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+        {"role": "system", "content": "You are a sales agent trying to present information regarding a company to someone who clicked an ad."},
+        {"role": "user", "content": prompt}
+        ],
+        stream=True
+    )
+    return response
+
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
+pinecone_key = st.text_input("pinecone API Key", type="password")
+if not openai_api_key or not pinecone_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
-
+    open_ai_client, pinecone = connections(openai_api_key, pinecone_key)
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
@@ -32,22 +71,25 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if query := st.chat_input("What is up?"):
 
         # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "user", "content": query})
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(query)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+        prompt = get_similar("ns1", query, open_ai_client, pinecone)
+        stream = get_completion(prompt, openai_api_key)
+
+        # # Generate a response using the OpenAI API.
+        # stream = client.chat.completions.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": m["role"], "content": m["content"]}
+        #         for m in st.session_state.messages
+        #     ],
+        #     stream=True,
+        # )
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
