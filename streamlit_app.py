009f17455f507dc61ffc7f39fe2b5f8c06332d71
siddadatrao <sidd.adatrao@gmail.com>
2024-07-26 09:45:51 -0700

    Bug where only retreived text was being passed as context and prompt. Fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def get_embedding(text, open_ai_client, model="text-embedding-3-small"):
    return open_ai_client.embeddings.create(input = [text], model=model).data[0].embedding
 
 def templatize(query, context):
-    return "Answer the question based only on the following context:" + query + "Question: " + query
+    return "Answer the question based only on the following context:" + context + "Question: " + query
 
 def get_similar(namespace, query, open_ai_client, pinecone):
     query_embedding = get_embedding(query, open_ai_client, model='text-embedding-3-small')
@@ -31,7 +31,7 @@ def get_similar(namespace, query, open_ai_client, pinecone):
         include_metadata=True,
     )
     query_result = query_results1['matches'][0]['metadata']['text']
-    return templatize(query_result, query)
+    return templatize(query, query_result)
 
 def get_completion(prompt, key):
     openai.api_key = key
